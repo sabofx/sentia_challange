5c04238c4669867f649350d40fd90e5a9a58d7c2
MEGA-FX\sabo-fx <github@sabo-fx.net>
2018-05-14 02:53:21 +0200

    Added comments, instructions and rollback script

--- deploy_challange_01.ps1
+++ deploy_challange_01.ps1
@@ -1,23 +1,42 @@
-﻿#https://github.com/sabofx/sentia_challange.git
+﻿######################################################
+# INSTRUCTIONS:                                      #
+#                                                    #
+# Make sure to download the latest files from:       #
+# https://github.com/sabofx/sentia_challange.git     #
+#                                                    #
+# Put all downloaded files in the same folder        #
+######################################################
+
+write-host "Setting assignment values"
+$strResourceGroup = "rgsentia"
+$arrResourceTypesAllowed = "Microsoft.Storage","Microsoft.Compute"
+
+write-host "Determine path of JSON files depending on folder from where script is launched"
+$ScriptRoot = Split-Path -Parent -Path $MyInvocation.MyCommand.Definition
+$TemplateFilepath = $("$ScriptRoot" + "\template.json")
+$TemplateParameterFilepath = $("$ScriptRoot" + "\parameters.json")
 
 Connect-AzureRmAccount
 
-New-AzureRmResourceGroup -Name rgsentia -Location "westeurope"
-New-AzureRmResourceGroupDeployment -ResourceGroupName rgsentia -TemplateFile template.json -TemplateParameterFile parameters.json
+write-host "Create Resource Group"
+New-AzureRmResourceGroup -Name $strResourceGroup -Location "westeurope"
 
+write-host "Deploy Resource Group based on local JSON files"
+New-AzureRmResourceGroupDeployment -ResourceGroupName $strResourceGroup -TemplateFile $TemplateFilepath -TemplateParameterFile $TemplateParameterFilepath
+
+write-host "Create TAGS"
 New-AzureRmTag -Name Environment -Value 'Test'
 New-AzureRmTag -Name Company -Value 'Sentia'
-Get-AzureRmResourceGroup rgsentia | Set-AzureRmResourceGroup -Tag @{Environment="Test";Company="Sentia"}
 
+write-host "Apply TAGS to Resource Group"
+Get-AzureRmResourceGroup $strResourceGroup | Set-AzureRmResourceGroup -Tag @{Environment="Test";Company="Sentia"}
+
+write-host "Define policy"
 $definition = New-AzureRmPolicyDefinition -Name "pd_allowed-resourcetypes" -DisplayName "Allowed resource types" -description "This policy enables you to specify the resource types that your organization can deploy." -Policy 'https://raw.githubusercontent.com/Azure/azure-policy/master/samples/built-in-policy/allowed-resourcetypes/azurepolicy.rules.json' -Parameter 'https://raw.githubusercontent.com/Azure/azure-policy/master/samples/built-in-policy/allowed-resourcetypes/azurepolicy.parameters.json' -Mode All
-# $definition
 
-
-$RG = Get-AzureRmResourceGroup -Name “rgsentia”
-$strScope = “/subscriptions/08533c31-8f8a-488e-9d95-e5f978a8f22b/resourceGroups/$($RG.ResourceGroupName)”
-
-$arrResourceTypesAllowed = "Microsoft.Storage","Microsoft.Compute"
-
+write-host "Apply policy"
+$strScope = (Get-AzureRmResourceGroup -Name $strResourceGroup).ResourceId
 $assignment = New-AzureRMPolicyAssignment -Name "pa_limit_resourcetypes" -Scope $strScope -listOfResourceTypesAllowed $arrResourceTypesAllowed -PolicyDefinition $definition
 $assignment
 
+write-host "Done!"
---
+++ rollback_deployment.ps1
@@ -0,0 +1,14 @@
+﻿$strResourceGroup = "rgsentia"
+
+Connect-AzureRmAccount
+
+# Unassign policy from resource group
+$RG = Get-AzureRmResourceGroup -Name $strResourceGroup
+Remove-AzureRmPolicyAssignment -name "pa_limit_resourcetypes" -scope $RG.resourceid
+
+# Remove policy
+$PolicyDefinition = Get-AzureRmPolicyDefinition -Name "pd_allowed-resourcetypes" 
+Remove-AzureRmPolicyDefinition -Id $PolicyDefinition.ResourceId -Force
+
+# Remove resource group
+Remove-AzureRmResourceGroup $strResourceGroup -force
